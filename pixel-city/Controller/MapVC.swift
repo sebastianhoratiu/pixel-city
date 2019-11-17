01962bd4c9dfc055e133871ae60ff12e251d508e
Sebastian Horatiu <sebastianhoratiu@yahoo.com>
2019-11-17 12:33:01 +0200

    lesson-99: Now tapping on the CenterMapBtn is actually centering the mapp arround our location. To be able to do this, we had to import CoreLocation, set is delegate to our MapVC, handle our authorisationStatus, set Privacy - Location... descriptions, set a desired region arround our current location in meters (1000 meters). Basically, if location status is nondetermined, we request location authorization; then when authorization status has changed (didChangeAuthorisationStatus) we are centering the map on our current location.

--- pixel-city/Controller/MapVC.swift
+++ pixel-city/Controller/MapVC.swift
@@ -8,22 +8,51 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapVC: UIViewController {
     
     @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManger = CLLocationManager()
+    let authorizationStatus = CLLocationManager.authorizationStatus()
+    let regionRadiusMeters: Double = 1000
 
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
+        locationManger.delegate = self
+        configureLocationServices()
     }
 
     @IBAction func CenterMapBtnWasPressed(_ sender: Any) {
+        if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse {
+            centerMapOnUserLocation()
+        }
     }
     
 
 }
 
 extension MapVC: MKMapViewDelegate {
+    func centerMapOnUserLocation() {
+        guard let currentLocationCoordinate = locationManger.location?.coordinate else { return }
+        let coordinateRegion = MKCoordinateRegion(center: currentLocationCoordinate, latitudinalMeters: regionRadiusMeters, longitudinalMeters: regionRadiusMeters)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
     
+}
+
+extension MapVC: CLLocationManagerDelegate {
+    func configureLocationServices() {
+        if authorizationStatus == .notDetermined {
+            locationManger.requestAlwaysAuthorization()
+        } else {
+            return
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        centerMapOnUserLocation()
+    }
 }
